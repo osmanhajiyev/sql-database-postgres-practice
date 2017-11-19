b88d68baf5a6bebcc023c968db33c478a282644c
osmanhajiyev <osman.hajiyev@gmail.com>
2017-11-19 12:30:13 -0800

    Success message displayed on successful queries

--- demoui/server/api/manager.js
+++ demoui/server/api/manager.js
@@ -100,7 +100,7 @@ router.post('/manager/fire', bodyParser.json(), function (req, res, next) {
     })
     .then(result => {
       // result[1] is the number of rows changed
-      res.send('/employee')
+      res.send('/manager/success')
     })
 })
 
@@ -130,7 +130,7 @@ router.post('/manager/hire', bodyParser.json(), function (req, res, next) {
     })
     .then(result => {
       // result[1] is the number of rows changed
-      res.send('/employee')
+      res.send('/manager/success')
     })
 })
 
@@ -166,6 +166,8 @@ router.post('/manager/edit', bodyParser.json(), function (req, res, next) {
   }
 
   query = query + 'WHERE employee_id = ' + id + ';';*/
+
+
   const query = 'UPDATE employee SET name = :name, union_id = '+ union + ', hourly_rate = '+rate+', occupation = :occupation, sin = '+sin+', dept_id = '+dept+' WHERE employee_id = '+id+' ;'
 
   connection.query(query,
@@ -183,7 +185,7 @@ router.post('/manager/edit', bodyParser.json(), function (req, res, next) {
     })
     .then(result => {
       // result[1] is the number of rows changed
-      res.send('/employee')
+      res.send('/manager/success')
     })
 })
 
